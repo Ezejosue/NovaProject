78e0955951d133af375728dcaf8ceea2391c4d41
Frank <staneyvasconcelos0@gmail.com>
2019-05-29 11:54:54 -0600

    modificacion del menu
    Actualice el menu con el scrollbar unicamente tiene el bug de no modificar contraseña

--- core/helpers/dashboard.php
+++ core/helpers/dashboard.php
@@ -65,40 +65,49 @@ class Dashboard
 										<i class="fas fa-tachometer-alt"></i>Vista General
 									</a>
 								</li>
-								<li>
-									<a href="categorias.php">
-										<i class="fas fa-list"></i>Categorías</a>
-								</li>
 										<li>
-											<a href="#itemsDrop" data-toggle="collapse" class="collapsed"><i class="fas fa-shopping-basket"></i><span>
+											<a href="#itemsDrop" data-toggle="collapse" class="collapsed"><i class="fab fa-dropbox"></i><span>
 											Productos</span></a>
 												<div id="itemsDrop" class="collapse">
 													<ul>
+													<li>
+															<a href="categorias.php">
+																<i class="fas fa-list"></i>Categorías</a>
+														</li>
 														<li>													
 															<a href="productos.php">
-															<i class="fas fa-shopping-basket"></i>Materia prima</a>
+															<i class="fas fa-cart-plus"></i>Materia prima</a>
 														</li>
 														<li>
 															<a href="platillos.php">
-															<i class="fas fa-shopping-basket"></i>Platillos</a>
+															<i class="fas fa-utensils"></i>Platillos</a>
 														</li>
 													</ul>
 												</div>
-                                    	</li>
+										</li>
 										<li>
+											<a href="#itemsDrop1" data-toggle="collapse" class="collapsed"><i class="fas fa-user-circle"></i><span>
+											Perfiles</span></a>
+												<div id="itemsDrop1" class="collapse">
+													<ul>
+													<li>
 											<a href="usuarios.php">
-												<i class="fas fa-users"></i>Usuarios</a>
+												<i class="fas fa-user-plus"></i>Usuarios</a>
 										</li>
 										<li>
 											<a href="tipo_usuarios.php">
-												<i class="fa fa-th	
-												"></i>Tipo de usuarios</a>
+											<i class="fas fa-users"></i>Tipo de usuarios</a>
 										</li>
 										<li>
 											<a href="empleados.php">
 												<i class="fas fa-id-card"></i>Empleados</a>
 										</li>
 									</li>
+													</ul>
+												</div>
+										</li>
+										
+										
 									<li>
 										<a href="reportes.php">
 											<i class="fas fa-chart-bar"></i>Reportes</a>
@@ -128,8 +137,11 @@ class Dashboard
 									<div class="header-button-item mr-0 js-sidebar-btn">
 										<i class="zmdi zmdi-menu"></i>
 									</div>
+									
 								</div>
+								
 							</div>
+							
 						</div>
 					</header>
 					<aside class="menu-sidebar2 js-right-sidebar d-block d-lg-none">
@@ -138,47 +150,55 @@ class Dashboard
 								<img src="../resources/img/logo.png" alt="PizzaNova" />
 							</a>
 						</div>
-						<div class="menu-sidebar2__content js-scrollbar2">
-							<div class="account2">
-								<div class="image img-cir img-120">
-									<img src="../resources/img/icon/avatar-big-01.jpg" alt="John Doe" />
-								</div>
-								<h4 class="name">john doe</h4>
-								<a href="index.php">Cerrar Sesión</a>
+						<div class="menu-sidebar2__content js-scrollbar1">
+						<div class="account2">
+							<div class="image img-cir img-120">
+								<img src="../resources/img/icon/avatar-big-01.jpg" alt="John Doe" />
 							</div>
+							<h4 class="name">john doe</h4>
+							<div class="row">
+								<a href="#modal-profile" class="modal-trigger" data-toggle="modal" onclick="modalProfile()"> <h6> <i class="fa fa-edit"></i> |</h6> </a>
+								<a href="#modal-password1" class="modal-trigger" data-toggle="modal"> <h6> | <i class="fa fa-key"></i> | </h6> </a>
+								<a href="#" onclick="signOff()"> <h6> | <i class="fa fa-sign-out-alt"></i></h6> </a>
+							</div>
+						</div>
 							<nav class="navbar-sidebar2">
-								<ul class="list-unstyled navbar__list">
-									<li>
-										<a href="inicio.php">
-											<i class="fas fa-tachometer-alt"></i>Vista General
-											<span class="arrow">
-
-											</span>
-										</a>
-									</li>
-									<li>
-										<a href="categorias.php">
-											<i class="fas fa-list"></i>Categorías</a>
-									</li>
-									<li>
-										<a href="productos.php">
-											<i class="fas fa-shopping-basket"></i>Productos</a>
-									</li>
-									<li>
-										<a href="usuarios.php">
-											<i class="fas fa-users"></i>Usuarios</a>
-									</li>
-									<li>
-										<a href="empleados.php">
-											<i class="fas fa-id-card"></i>Empleados</a>
-									</li>
-									</li>
-									<li>
-										<a href="reportes.php">
-											<i class="fas fa-chart-bar"></i>Reportes</a>
-									</li>
-								</ul>
-							</nav>
+							<ul class="list-unstyled navbar__list">
+								<li>
+									<a href="inicio.php">
+										<i class="fas fa-tachometer-alt"></i>Vista General
+									</a>
+								</li>
+								<li>
+									<a href="categorias.php">
+										<i class="fas fa-list"></i>Categorías</a>
+								</li>
+								<li>													
+									<a href="productos.php">
+									<i class="fas fa-cart-plus"></i>Materia prima</a>
+								</li>
+								<li>
+									<a href="platillos.php">
+									<i class="fas fa-utensils"></i>Platillos</a>
+								</li>
+								<li>
+									<a href="usuarios.php">
+										<i class="fas fa-user-plus"></i>Usuarios</a>
+								</li>
+								<li>
+									<a href="tipo_usuarios.php">
+									<i class="fas fa-users"></i>Tipo de usuarios</a>
+								</li>
+								<li>
+									<a href="empleados.php">
+										<i class="fas fa-id-card"></i>Empleados</a>
+								</li>
+								<li>
+									<a href="reportes.php">
+										<i class="fas fa-chart-bar"></i>Reportes</a>
+								</li>
+							</ul>
+						</nav>
 						</div>
 					</aside>
 					<!-- Fin Menu Responsive-->
@@ -368,7 +388,69 @@ class Dashboard
 							</div>
 						</form>
 					</div>
+				</div>
 			</div>
+
+			<div id="modal-password1" class="modal fade">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title">CAMBIAR CONTRASEÑA</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+						</div>
+						<form method="post" id="form-password1">
+							<div class="modal-body">
+								<div class="row">
+									<div class="col-sm-1">
+										<i class="fa fa-user"></i>
+									</div>
+									<div class="col-sm-11">
+                            			<input id="clave_actual" type="password" name="clave_actual" class="form-control validate" required placeholder="Clave Actual">
+									</div>
+								</div>
+							</div>
+							<div class="modal-body">
+								<div class="row">
+									<div class="col-sm-1">
+										<i class="fa fa-user"></i>
+									</div>
+									<div class="col-sm-11">
+                            			<input id="clave_actual1" type="password" name="clave_actual1" class="form-control validate" required placeholder="Confirmar Clave">
+									</div>
+								</div>
+							</div>
+							<div class="modal-body">
+								<div class="row">
+									<div class="col-sm-1">
+										<i class="fa fa-user"></i>
+									</div>
+									<div class="col-sm-11">
+                            			<input id="clave_nueva" type="password" name="clave_nueva" class="form-control validate" required placeholder="Nueva Clave">
+									</div>
+								</div>
+							</div>
+							<div class="modal-body">
+								<div class="row">
+									<div class="col-sm-1">
+										<i class="fa fa-user"></i>
+									</div>
+									<div class="col-sm-11">
+                            			<input id="clave_nueva1" type="password" name="clave_nueva1" class="form-control validate" required placeholder="Confirmar Clave">
+									</div>
+								</div>
+							</div>
+								<div class="modal-body text-center">
+                    				<button type="button" class="btn btn-secondary tooltipped" data-tooltip="Cancelar" data-dismiss="modal">Cancelar</button>
+                    				<button type="submit" class="btn btn-primary tooltipped" data-tooltip="Cambiar">Guardar</button>
+                				</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
 		');
 	}
 }
