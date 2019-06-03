f78a525945988848e7f027a3b5794346eac10045
Ezejosue <aezequiel11@hotmail.com>
2019-06-03 12:05:05 -0600

    Update-empleados

--- views/empleados.php
+++ views/empleados.php
@@ -76,8 +76,8 @@
                                 <i class="fas fa-id-badge"></i>
                             </div>
                             <div class="col-sm-11">
-                                <input id="create_dui" type="text" name="create_dui" class="validate form-control" validate min="00000000"
-                                    placeholder="00000000-0" required>
+                                <input id="create_dui" type="text" name="create_dui" class="validate form-control"
+                                    validate min="00000000" placeholder="00000000-0" required>
                             </div>
                         </div>
                     </div>
@@ -99,29 +99,30 @@
                             </div>
                             <div class="col-sm-11">
                                 <input id="create_telefono" type="text" name="create_telefono"
-                                    class="validate form-control" validate min="00000000" placeholder="00000000" required>
+                                    class="validate form-control" validate min="00000000" placeholder="00000000"
+                                    required>
                             </div>
                         </div>
                     </div>
                     <div class="modal-body">
                         <div class="row">
                             <div class="col-sm-1">
-                            <i class="fas fa-venus-mars"></i>
+                                <i class="fas fa-venus-mars"></i>
                             </div>
                             <div class="col-sm-11">
-                                <input id="create_genero" type="text" name="create_genero"
-                                    class="validate form-control" placeholder="M/F" required>
+                                <input id="create_genero" type="text" name="create_genero" class="validate form-control"
+                                    placeholder="M/F" required>
                             </div>
                         </div>
                     </div>
                     <div class="modal-body">
                         <div class="row">
                             <div class="col-sm-1">
-                            <i class="fas fa-venus-mars"></i>
+                                <i class="fas fa-venus-mars"></i>
                             </div>
                             <div class="col-sm-11">
-                                <input id="create_fecha" type="date" name="create_fecha"
-                                    class="validate form-control" placeholder="" required>
+                                <input id="create_fecha" type="date" name="create_fecha" class="validate form-control"
+                                    placeholder="" required>
                             </div>
                         </div>
                     </div>
@@ -178,80 +179,149 @@
         </div>
     </div>
     <!-- Modal de Modificar -->
-    <div id="modal-update" class="modal fade">
+    <div class="modal fade" id="modal-update">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">MODIFICAR PRODUCTO</h5>
+                    <h5 class="modal-title">MODIFICAR EMPLEADO</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <form method="post" id="form-update" enctype="multipart/form-data">
-                    <input type="hidden" id="foto_usuario" name="foto_usuario" />
-                    <input type="hidden" id="id_usuario" name="id_usuario" />
+                    <input type="hidden" id="id_empleado" name="id_empleado" />
                     <div class="modal-body">
-                        <div class="row">
-                            <div class="col-sm-1">
-                                <i class="fa fa-user"></i>
-                            </div>
-                            <div class="col-sm-11">
-
-                                <input id="update_alias" type="text" name="update_alias" class="validate form-control"
-                                    placeholder="Nombre De Usuario" required>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col-sm-1">
-                                <i class="fas fa-users"></i>
-                            </div>
-                            <div class="col-sm-11">
-                                <select id="update_tipo" name="update_tipo" class="form-control">
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col-sm-1">
-                                <i class="fa fa-image"></i>
-                            </div>
-                            <div class="col-sm-11">
-                                <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="update_archivo"
-                                        name="update_archivo">
-                                    <label class="custom-file-label" for="update_archivo">Escoga un archivo</label>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="fa fa-user"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <input id="update_nombre" type="text" name="update_nombre"
+                                        class="validate form-control" placeholder="Nombre De Empleado" required>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col-sm-1">
-                                <i class="fa fa-eye-slash"></i>
-                            </div>
-                            <div class="col-sm-11">
-                                <div class="custom-control custom-switch">
-                                    <input type="checkbox" class="custom-control-input" id="update_estado"
-                                        name="update_estado">
-
-                                    <label class="custom-control-label" for="update_estado">
-                                        <i class="fa fa-eye"></i>
-                                    </label>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="fa fa-user"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <input id="update_apellido" type="text" name="update_apellido"
+                                        class="validate form-control" placeholder="Apellido" required>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="modal-body text-center">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                        <button type="submit" class="btn btn-primary tooltipped" data-tooltip="Crear">Aceptar</button>
-                    </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="fas fa-id-badge"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <input id="update_dui" type="text" name="update_dui" class="validate form-control"
+                                        validate min="00000000" placeholder="00000000-0" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="fas fa-street-view"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <input id="update_direccion" type="text" name="update_direccion"
+                                        class="validate form-control" placeholder="Dirección" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="fas fa-id-badge"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <input id="update_telefono" type="text" name="update_telefono"
+                                        class="validate form-control" validate min="00000000" placeholder="00000000"
+                                        required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="fas fa-venus-mars"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <input id="update_genero" type="text" name="update_genero"
+                                        class="validate form-control" placeholder="M/F" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="fas fa-venus-mars"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <input id="update_fecha" type="date" name="update_fecha"
+                                        class="validate form-control" placeholder="" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="fab fa-font-awesome-flag"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <input id="update_nacionalidad" type="text" name="update_nacionalidad"
+                                        class="validate form-control" placeholder="Nacionalidad" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="fas fa-at"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <input id="update_email" type="text" name="update_email"
+                                        class="validate form-control" placeholder="@mail.com" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="fas fa-male"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <select id="create_cargo" name="create_cargo" class="form-control">
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-sm-1">
+                                    <i class="fas fa-user"></i>
+                                </div>
+                                <div class="col-sm-11">
+                                    <select id="create_usuario" name="create_usuario" class="form-control">
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-body text-center">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                            <button type="submit" class="btn btn-primary tooltipped"
+                                data-tooltip="Crear">Aceptar</button>
+                        </div>
                 </form>
             </div>
         </div>
     </div>
-<?php
+    <?php
 Dashboard::footerTemplate('empleados.js', '#tabla-empleados');
 ?>
