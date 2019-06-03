c03c025b5549b53ba706259c3bf688d91dccc7b3
Carlos Quijano <quijanoo12345@gmail.com>
2019-06-03 11:59:13 -0600

    creando crud de platillos

--- core/models/platillos.php
+++ core/models/platillos.php
@@ -1,15 +1,16 @@
 <?php
-class Categorias extends Validator
+class Platillos extends Validator
 {
 	// Declaración de propiedades
 	private $id = null;
 	private $nombre = null;
 	private $imagen = null;
-	private $descripcion = null;
-	private $estado = null;
+	private $precio = null;
+	private $id_categotia = null;
+	private $id_receta = null;
 	private $ruta = '../../resources/img/categorias/';
 
-	// Métodos para sobrecarga de propiedades
+	// Métodos para sobrecarga de propiedades de el metodo platillos
 	public function setId($value)
 	{
 		if ($this->validateId($value)) {
@@ -60,75 +61,110 @@ class Categorias extends Validator
 		return $this->ruta;
 	}
 
-	public function setDescripcion($value)
+	public function setPrecio($value)
 	{
 		if ($value) {
-			if ($this->validateAlphanumeric($value, 1, 200)) {
-				$this->descripcion = $value;
+			if ($this->validateMoney($value)) {
+				$this->precio = $value;
 				return true;
 			} else {
 				return false;
 			}
 		} else {
-			$this->descripcion = null;
+			$this->precio = null;
 			return true;
 		}
 	}
 
-	public function getDescripcion()
+	public function getPrecio()
 	{
-		return $this->descripcion;
+		return $this->precio;
 	}
-	
-	public function setEstado($value)
+	public function setCategoria($value)
 	{
-		if ($value == 0 || $value == 1) {
-			$this->estado = $value;
-			return true;
-		} else {
+		if($this->validateId($value)){
+			$this->id_categoria=$value;
+			return true;	
+		}else{
 			return false;
 		}
 	}
-
-	public function getEstado()
-	{
-		return $this->estado;
-	}
-
-	// Metodos para el manejo del SCRUD
-	public function readCategorias()
-	{
-		$sql = 'SELECT id_categoria, nombre_categoria, descripcion, foto_categoria, estado FROM categorias ORDER BY nombre_categoria';
-		$params = array(null);
-		return conexion::getRows($sql, $params);
-	}
-
-	public function createCategoria()
-	{
-		$sql = 'INSERT INTO categorias(nombre_categoria, descripcion, foto_categoria, estado) VALUES(?, ?, ?, ?)';
-		$params = array($this->nombre, $this->descripcion, $this->imagen, $this->estado);
-		return conexion::executeRow($sql, $params);
-	}
-
 	public function getCategoria()
 	{
-		$sql = 'SELECT id_categoria, nombre_categoria, descripcion, foto_categoria, estado FROM categorias WHERE id_categoria = ?';
-		$params = array($this->id);
-		return conexion::getRow($sql, $params);
+		return $this->id_categoria;
 	}
 
-	public function updateCategoria()
+	public functoin setReceta($value)
 	{
-		$sql = 'UPDATE categorias SET nombre_categoria = ?, descripcion = ?, foto_categoria = ?, estado=? WHERE id_categoria = ?';
-		$params = array($this->nombre,  $this->descripcion, $this->imagen, $this->estado, $this->id);
-		return conexion::executeRow($sql, $params);
+		if($this->validateId($value)){
+			$this->id_receta= $value;
+			return true;
+		}else{
+			return false;
+		}
+
+	}
+	public function getReceta()
+	{
+		return $this->id_receta
 	}
 
-	public function deleteCategoria()
-	{
-		$sql = 'DELETE FROM categorias WHERE id_categoria = ?';
-		$params = array($this->id);
-		return conexion::executeRow($sql, $params);
-	}
+
+	// Metodos para el manejo del SCRUD
+	public function createPlatillo()
+    {
+        $sql = 'INSERT INTO Platillos(nombre_platillo, precio, imagen, id_categoria, id_receta) VALUES (?, ?, ?, ?, ?)';
+        $params = array($this->nombre_platillo, $this->precio, $this->imagen, $this->id_categoria, $this->id_receta);
+        return Conexion::executeRow($sql, $params);
+    }
+
+    public function getPlatillo()
+    {
+        $sql = 'SELECT id_platillo, nombre_platillo, imagen, precio, id_categoria, id_receta FROM Platillos WHERE id_platillo = ?';
+        $params = array($this ->id);
+        return Conexion::getRow($sql, $params);
+    }
+
+    public function readPlatillo()
+    {
+        $sql = 'SELECT  id_platillo, nombre_platillo FROM Platillos ORDER BY nombre_platillo';
+		$params = array($this->id_platillo);
+		return Conexion::getRows($sql, $params);
+    }
+
+    public function readPlatillo()
+    {
+        $sql = 'SELECT id_platillo, nombre_platillo, precio, imagen From Platillos ORDER BY nombre_platillo';
+        $params = array(null);
+        return Conexion::getRows($sql, $params);
+    }
+
+    public function searchEmpleados()
+    {
+        $sql = 'SELECT * FROM Platillos WHERE nombre_platillo LIKE ? OR precio';
+        $params = array("%$value%", "%$value%");
+        return Conexion::getRows($sql, $params);
+    }
+
+    public function readCategoria()
+    {
+        $sql = 'SELECT id_categoria, nombre_categoria FROM Categorias';
+        $params = array(null);
+        return Conexion::getRows($sql, $params);
+    }
+
+    public function updatePlatillo()
+    {
+        $sql = 'UPDATE Empleados SET nombre_empleado = ?, apellido_empleado = ?, dui = ?, direccion = ?, telefono = ?, genero = ?, fecha_nacimiento = ?, nacionalidad = ?, correo = ?, id_cargo = ?, id_usuario = ? WHERE id_empleado = ?';
+        $params = array($this->nombre_empleado, $this->apellido_empleado, $this->dui, $this->direccion, $this->telefono, $this->genero, $this->fecha_nacimiento, $this->nacionalidad, $this->correo, $this->id_cargo, $this->id_usuario, $this->id);
+        return Conexion::executeRow($sql, $params);
+    }
+
+    public function deletePlatillo()
+    {
+        $sql = 'DELETE FROM Platillos WHERE id_platillo = ?';
+        $params = array($this->id);
+        return Conexion::executeRow($sql, $params);
+    }
 }
 ?>
