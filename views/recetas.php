5b200c98c0ad482ef7921a671a1f9c1ebcc08cae
Riverapjj <keko.rai.jr@gmail.com>
2019-06-04 15:55:03 -0600

    merge

---
+++ views/recetas.php
@@ -0,0 +1,180 @@
+<!-- SIDEBAR-->
+<?php
+    require_once('../core/helpers/dashboard.php');
+    Dashboard::headerTemplate('Recetas');
+?>
+<!-- Contenido-->
+<div class="main-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-3">
+                <a href="#modal-create" class="btn btn-success tooltipped modal-trigger" data-toggle="modal"
+                    data-tooltip="Agregar">
+                    <span class="btn-label">
+                        <i class="fa fa-plus"></i>
+                    </span>
+                </a>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="container">
+                <table class="display" id="tabla-recetas">
+                    <thead>
+                        <tr>
+                            <th>NOMBRE</th>
+                            <th>TIEMPO</th>
+                            <th>ELABORACIÓN</th>
+                            <th>CATEGORIA</th>
+                            <th>MATERIA PRIMA</th>
+                            <th>MEDIDA</th>
+                            <th>ACCIÓN</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tbody-read">
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!-- Modals-->
+    <!-- Modal de Agregar -->
+    <div class="modal fade" id="modal-create">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">AGREGAR RECETAS</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form method="post" id="form-create">
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-sm-1">
+                                <i class="fa fa-list"></i>
+                            </div>
+                            <div class="col-sm-11">
+                                <input placeholder="Nombre de receta" class="form-control" id="create_nombre" name="create_nombre"
+                                    for="nombre_receta">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-sm-1">
+                                <i class="fa fa-file-alt"></i>
+                            </div>
+                            <div class="col-sm-11">
+                                <textarea placeholder="Tiempo de elaboración" class="form-control" id="create_tiempo"
+                                    name="create_tiempo" for="tiempo" rows="3"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-sm-1">
+                                <i class="fa fa-eye-slash"></i>
+                            </div>
+                            <div class="col-sm-11">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="create_estado"
+                                        name="create_estado">
+                                    <label class="custom-control-label" for="create_estado">
+                                        <i class="fa fa-eye"></i>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-body">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-primary">Aceptar</button>
+                    </div>
+            </div>
+        </div>
+        </form>
+    </div>
+    <!-- Modal de Modificar -->
+    <div id="modal-update" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">MODIFICAR CATEGORIAS</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form method="post" id="form-update">
+                    <input type="hidden" id="id_tipo_usuario" name="id_tipo_usuario" />
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-sm-1">
+                                <i class="fa fa-user"></i>
+                            </div>
+                            <div class="col-sm-11">
+                                <input id="update_nombre_tipo" type="text" name="update_nombre_tipo"
+                                    class="validate form-control" placeholder="Tipo de usuario" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-sm-1">
+                                <i class="fa fa-user"></i>
+                            </div>
+                            <div class="col-sm-11">
+
+                                <input id="update_descripcion" type="text" name="update_descripcion"
+                                    class="validate form-control" placeholder="Descripcion" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-sm-1">
+                                <i class="fa fa-eye-slash"></i>
+                            </div>
+                            <div class="col-sm-11">
+                                <div class="custom-control custom-switch">
+                                    <input type="checkbox" class="custom-control-input" id="update_estado"
+                                        name="update_estado">
+
+                                    <label class="custom-control-label" for="update_estado">
+                                        <i class="fa fa-eye"></i>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-body text-center">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-primary tooltipped" data-tooltip="Crear">Aceptar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Modal de Eliminar -->
+    <div class="modal fade" id="ventana3">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">ELIMINAR CATEGORIA</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <h6>¿Está seguro de que desea eliminar esta categoria?</h6>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                    <button type="button" class="btn btn-primary">Aceptar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+Dashboard::footerTemplate('recetas.js', '#tabla-recetas');
+?>
