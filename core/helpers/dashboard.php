490b381fd0f4fd1187d07281ea15edc55caa39d0
Carlos <quijanoo12345@gmail.com>
2019-06-04 10:02:24 -0600

    Validaciones en usuarios y en modificar datos del usuario

--- core/helpers/dashboard.php
+++ core/helpers/dashboard.php
@@ -242,6 +242,12 @@ class Dashboard
             <script type="text/javascript" src="../core/controllers/account.js"></script>
 			<script type="text/javascript" src="../core/controllers/'.$controller.'"></script>
 			<script src="../resources/js/bootstrap-validate.js"></script>
+			<script>bootstrapValidate("#clave_actual_1", "min:6:Ingrese una contraseña mayor a 5 caracteres")</script>
+			<script>bootstrapValidate("#clave_actual_2", "min:6:Ingrese una contraseña mayor a 5 caracteres")</script>
+			<script>bootstrapValidate("#clave_nueva_1", "min:6:Ingrese una contraseña mayor a 5 caracteres")</script>
+			<script>bootstrapValidate("#clave_nueva_2", "min:6:Ingrese una contraseña mayor a 5 caracteres")</script>
+			<script>bootstrapValidate("#profile_alias", "required:Ingrese un nombre de usuario")</script>
+			
 		');
 	}
 	public static function inicio()
@@ -304,8 +310,9 @@ class Dashboard
 									<div class="col-sm-11">
 										<div class="custom-file">
 											<input type="file" class="custom-file-input" id="profile_foto"
-												name="profile_foto">
+												name="profile_foto" required>
 											<label class="custom-file-label" for="profile_foto">Escoga un archivo</label>
+											<div class="invalid-feedback">Example invalid custom file feedback</div>
 										</div>
 									</div>
 								</div>
--- views/usuarios.php
+++ views/usuarios.php
@@ -145,7 +145,7 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form method="post" id="form-update" enctype="multipart/form-data">
+                <form class="was-validated" method="post" id="form-update" enctype="multipart/form-data">
                     <input type="hidden" id="foto_usuario" name="foto_usuario" />
                     <input type="hidden" id="id_usuario" name="id_usuario" />
                     <div class="modal-body">
@@ -179,8 +179,9 @@
                             <div class="col-sm-11">
                                 <div class="custom-file">
                                     <input type="file" class="custom-file-input" id="imagen_usuario"
-                                        name="imagen_usuario">
+                                        name="imagen_usuario" required>
                                     <label class="custom-file-label" for="imagen_usuario">Escoga un archivo</label>
+                                    <div class="invalid-feedback">Example invalid custom file feedback</div>
                                 </div>
                             </div>
                         </div>
@@ -210,12 +211,14 @@
             </div>
         </div>
     </div>
-    <?php
+
+<?php
 Dashboard::footerTemplate('usuarios.js', '#tabla-usuarios');
 ?>
 
     <script>bootstrapValidate("#create_clave1", "min:6:Ingrese una contraseña mayor a 5 caracteres")</script>
     <script>bootstrapValidate("#create_clave2", "min:6:Ingrese una contraseña mayor a 5 caracteres")</script>
+    <script>bootstrapValidate('#create_alias', 'required:Ingrese un nombre de usuario')</script>
 
     </body>
 
