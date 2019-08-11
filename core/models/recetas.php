9807532c189f80b39c64076c6b9ccde8fb6e7e89
Josué Rivera <keko.rai.jr@gmai.com>
2019-08-11 16:29:58 -0600

    arreglando db par funcionamiento de recetas

--- core/models/recetas.php
+++ core/models/recetas.php
@@ -200,7 +200,7 @@ class Recetas extends Validator
 	{
 		$sql = 'SELECT e.id_receta, nombre_receta, tiempo, CONCAT(nombre_materia, " (", descripcion, ")") AS MateriaPrima, e.cantidad 
 		FROM receta r INNER JOIN elaboraciones e USING(id_receta) 
-		INNER JOIN materiasprimas m USING(idMateria) WHERE id_receta = ? ';
+		INNER JOIN materiasprimas m USING(idMateria) WHERE id_receta = ? '	;
 		$params = array($this->idreceta);
 		return conexion::executeRow($sql, $params);
 	}
