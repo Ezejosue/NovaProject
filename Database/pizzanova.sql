0ddb1471dbbf529b7698e769026619179f838227
Josué Rivera <keko.rai.jr@gmai.com>
2019-09-09 08:15:05 -0600

    base actualizada

--- Database/pizzanova.sql
+++ Database/pizzanova.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
+--   
 -- Base de datos: `pizzanova`
 --
 CREATE DATABASE IF NOT EXISTS `pizzanova` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
