bd2ca6eb246d762068a21dd9c32178129ba154a3
Gerardo Ramirez <gerardogo145@gmail.com>
2019-09-16 09:43:23 -0600

    Quitando la impresión del menú

--- core/api/usuarios.php
+++ core/api/usuarios.php
@@ -651,7 +651,6 @@ if (isset($_GET['action'])) {
                                     $_SESSION['tipoUsuario'] = $usuario->getTipo_usuario();
                                     //Se llena la variable 'vistas' con las opciones del menú, según el tipo de usuario que ha iniciado sesión
                                     $_SESSION['vistas'] = $usuario->readMenu();
-                                    var_dump($_SESSION['vistas']);
                                     $_SESSION['tiempo'] = time();
                                     $result['status'] = 1;
                                 } else {
