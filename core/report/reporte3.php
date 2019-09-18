ef57a1239deba2711fce2512e916ce1712d155b9
G3rardo503 <gerardogo145@gmail.com>
2019-09-18 10:23:57 -0600

    Validando reporte3

--- core/report/reporte3.php
+++ core/report/reporte3.php
@@ -15,7 +15,6 @@ $pdf->SetAutoPageBreak(true,20);
 $pdf->addPage();
 $materia = new Materias();
 $pdf->SetFont('Arial','B',10);
-$data = $materia->readMeteriaCategoria();
 $pdf->Ln();
 $pdf->setX(60);
 // Cell(ancho, Alto, texto, borde, salto de linea, alineacion de texto)
@@ -26,8 +25,10 @@ $pdf->setX(38);
 $pdf->Ln();
 $categoria = '';
 
-//Comienza a crear las filas de productos según la consulta mysql del modelo
-foreach($data as $datos){
+if($materia->readMeteriaCategoria()) {
+  $data = $materia->readMeteriaCategoria();
+  //Comienza a crear las filas de productos según la consulta mysql del modelo
+  foreach($data as $datos) {
     if(utf8_decode($datos['nombre_categoria']) != $categoria){
         //Se coloca el color del fondo de las celdas en formato rgb
         $pdf->SetFillColor(239, 127, 26);
@@ -40,8 +41,9 @@ foreach($data as $datos){
         $pdf->Cell(155,10, utf8_decode($datos['nombre_categoria']),1,0,'C',true);
         $pdf->Ln();        
         $pdf->setX(30);
-        $pdf->Cell(135,10, utf8_decode('Producto'),1,0,'C');
         $pdf->Cell(20,10, utf8_decode('Cantidad'),1,0,'C');
+        $pdf->Cell(135,10, utf8_decode('Producto'),1,0,'C');
+        
         $categoria = $datos['nombre_categoria'];
         //saldo de linea
         $pdf->Ln();
@@ -50,10 +52,16 @@ foreach($data as $datos){
         $pdf->setX(30);
           // Cell(ancho, Alto, texto, borde, salto de linea, alineación de texto, color)
         //convertimos el texto a utf8
-        $pdf->Cell(135,10, utf8_decode($datos['nombre_materia']),1,0,'C');
         $pdf->Cell(20,10, utf8_decode($datos['cantidad']),1,0,'C');
+        $pdf->Cell(135,10, utf8_decode($datos['nombre_materia']),1,0,'C');
+        
         $pdf->Ln();
+  }
+    
+} else {
+  $pdf->Cell(175,5, utf8_decode('NO HAY DATOS REGISTRADOS'), 0, 0, 'C');
 }
+
 
 
 $pdf->AliasNbPages();
